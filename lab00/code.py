efe5b24d4f76e3b4df2a0cbc5386838b63dc5f3e
renjunbin <renjunbin123@126.com>
2023-12-04 15:21:15 +0800

    modified:   lab00/code.py

--- lab00/code.py
+++ lab00/code.py
@@ -7,12 +7,12 @@ def get_airspeed_velocity_of(unladen_swallow):
 # pretend there's code here...
 
 def fizzbuzz(num):
-	if num % 3 == 0: # edit this line
+	if num % 5 == 0 and num % 3 == 0:
+		print(f"{num}: fizzbuzz")
+	elif num % 3 == 0: # edit this line
 		print(f"{num}: fizz")
 	elif num % 5 == 0: # edit this line
 		print(f"{num}: buzz")
-	elif num % 5 == 0 and num % 3 == 0:
-		pass
 
 for i in range(1, 20):
 	fizzbuzz(i)
