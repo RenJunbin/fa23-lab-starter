30ae72891727714e84ac165f86c8c11fc729a1e6
renjunbin <renjunbin123@126.com>
2023-12-06 13:16:13 +0800

    modified:   ex10_ll_cycle.c 	modified:   ex9_double_pointers.c

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -3,4 +3,16 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+		if (!head)
+			goto _EXIT;
+			
+		node *fast_ptr = head->next ? head->next->next : head->next;
+		node *slow_ptr = head->next;
+
+		for (; fast_ptr != NULL && fast_ptr->next != NULL; fast_ptr = fast_ptr->next->next, slow_ptr = slow_ptr->next) {
+				if (fast_ptr == slow_ptr)
+						return 1;
+		}
+		_EXIT:
+			return 0;
 }
